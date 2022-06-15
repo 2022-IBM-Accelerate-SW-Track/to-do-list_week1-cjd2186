9c6b49df0e5361efb2c648170b43f3d1d47a3f3c
unknown <cjd2186@columbia.edu>
2022-06-15 06:48:51 -0400

    new changes, working webpage

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
-  
+
+import "./About.css";
+import chris_prof_pic from "../assets/chris_prof_pic.jpg";
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div class="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+              // Image goes here
+              src = {chris_prof_pic}
+              alt = "Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title"> Christopher Demirjian</div>
+            <div className="brief_description">
+              Christopher grew up in Westchester New York and currently studies computer science at Columbia University in New York City.
+            </div>
+            <div className="brief_description">
+              Christopher loves animals, and has two dogs!
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
